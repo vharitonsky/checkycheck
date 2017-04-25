a8ca7863b43ac8cc66931092be7f3ab9d186b89d
v.haritonskiy <v.haritonskiy@smartweb.com.ua>
2017-04-25 22:50:24 +0300

    add add

--- things.py
+++ things.py
@@ -1,12 +1,23 @@
 # things.py
 
 # Let's get this party started!
+import os
 import falcon
+from pymongo import MongoClient
 
 
 # Falcon follows the REST architectural style, meaning (among
 # other things) that you think in terms of resources and state
 # transitions, which map to HTTP verbs.
+mclient = MongoClient(os.environ['MONGODB_URI'])
+
+class AddResource(object):
+    def on_add(self, req, resp):
+        db = mclient['test-database']
+        db.counter.insert({})
+        resp.status = falcon.HTTP_200
+        resp.body = str(db.counter.count())
+
 class ThingsResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
@@ -21,6 +32,8 @@ app = falcon.API()
 
 # Resources are represented by long-lived class instances
 things = ThingsResource()
+add = AddResource()
 
 # things will handle all requests to the '/things' URL path
+app.add_route('/add', add)
 app.add_route('/things', things)
